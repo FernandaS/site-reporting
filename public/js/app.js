9c8dd0998fcb8ffc9d593eaf4f17ba04c2ebbaa7
kajdav <poppa.bear.joe@gmail.com>
2014-11-11 11:21:06 -0700

    Adds routes.

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,28 @@
 var app = angular.module('lds-report', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
-
-
-
-
-
-
-
-
-
-
+	$routeProvider
+		.when('/', {
+			templateUrl: '/views/home/home.html',
+			controller: 'homeCtrl'
+		})
+		.when('/login', {
+			templateUrl: '/views/login/login.html',
+			controller: 'loginCtrl'
+		})
+		.when('/manage', {
+			templateUrl: '/views/manage/manage.html',
+			controller: 'manageCtrl'
+		})
+		.when('/reports', {
+			templateUrl: '/views/reports/reports.html',
+			controller: 'reportsCtrl'
+		})
+		.when('/trends', {
+			templateUrl: '/views/trends/trends.html',
+			controller: 'trendCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
 }])
