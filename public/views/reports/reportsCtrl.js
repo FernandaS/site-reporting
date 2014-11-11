c57ccfddd94c46007c1256d26e8c79d07a6ee16c
jacob-israel-turner <poppa.bear.joe@gmail.com>
2014-11-11 15:55:55 -0700

    Script tags, routes, reports view.

--- public/views/reports/reportsCtrl.js
+++ public/views/reports/reportsCtrl.js
@@ -1,5 +1,18 @@
 var app = angular.module('lds-report');
 
-app.controller('reportsCtrl', function($scope){
+app.controller('reportsCtrl', function($scope, reportService, centerService){
+	$scope.reports = [];
+	$scope.centers = [];
 
+	$scope.years = [2011, 2012, 2013, 2014];
+	$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	var date = new Date();
+	$scope.year = date.getFullYear();
+	$scope.month = date.getMonth();
+	// reportService.getListBy(month, year).then(function(data){
+	// 	$scope.reports = data.data;
+	// })
+	// centerService.getAll().then(function(data){
+	// 	$scope.centers = data.data;
+	// })
 });
