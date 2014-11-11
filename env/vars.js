37694f42c694e7776928546e6df4083d00dbe1ff
Aaron Rumery <bloodhawk@users.noreply.github.com>
2014-11-11 12:01:37 -0700

    fixes vars.js

--- env/vars.js
+++ env/vars.js
@@ -1,8 +1,8 @@
 module.exports = {
 	expressSecret: 'sadkasd asdkasd ae213214ksad',
 	expressPort: 9001,
-	dbHost = '104.236.13.205',
-	dbUser = 'lds',
-	dbPass = 'hartwell1',
-	dbName = 'visitorcenter'
+	dbHost: '104.236.13.205',
+	dbUser: 'lds',
+	dbPass: 'hartwell1',
+	dbName: 'visitorcenter'
 }
