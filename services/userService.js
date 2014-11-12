74c8a5bdd9a914878ed58c16f1761d3f8fde4323
Jason Turner <jasonalmaturner@gmail.com>
2014-11-12 15:32:31 -0700

    added user and center endpoints

--- services/userService.js
+++ services/userService.js
@@ -6,9 +6,11 @@ var services = {
 	addUser: addUser
 };
 
-module.exports = function(){
-	return services;
-};
+// module.exports = function(){
+// 	return services;
+// };
+
+module.exports = services;
 
 //logic
 function addUser(uData){
