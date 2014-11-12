df939be22d7d74114ab2613d8da83c1514fc3e77
jacob-israel-turner <poppa.bear.joe@gmail.com>
2014-11-12 15:28:36 -0700

    Completes center submit.  We have contact from front to back.

--- public/views/manage/centers/center-control.js
+++ public/views/manage/centers/center-control.js
@@ -2,10 +2,15 @@ angular.module('lds-report')
 	.controller('centerCtrl', submitReportCtrl)
 
 function submitReportCtrl($scope, centerService){
+	$scope.yes = 'YES';
+	$scope.no = 'NO';
+	$scope.newCenter = {};
+	$scope.newCenter.active = 'YES';
 	$scope.test = 'Thisis a test.....'
 	$scope.submitCenter = function(){
 		centerService.create($scope.newCenter).then(function(data){
 			console.log(data);
 		})
+		// console.log($scope.newCenter)
 	}
 }
