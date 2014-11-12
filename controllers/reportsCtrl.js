63d4bcf350e4b511899a858f7eeb98f4becc14fa
Jason Turner <jasonalmaturner@gmail.com>
2014-11-12 12:08:44 -0700

    added reportCtrl

--- controllers/reportsCtrl.js
+++ controllers/reportsCtrl.js
@@ -1,12 +1,11 @@
-var reportService = require('./services');
+var reportService = require('./services/reportService');
 
 module.exports = {
 // remove console.logs after endpoints are working.
 // rename reportService functions as necessary.
+// parse out the req into the obj once I get keys from Aaron
 	getReport: function (req, res) {
-		var obj = {
-
-		}
+		var obj = {}
 		reportsService.getReport(obj).then(function(err, res) {
 			if(err) {
 				console.log(err);
@@ -58,17 +57,43 @@ module.exports = {
 		});
 	},
 
-	getRport: function (req, res) {
+	getReportsList: function (req, res) {
 		var obj = {}
-		reportsService.deleteReport(obj).then(function(err, res) {
+		reportsService.getReportsList(obj).then(function(err, res) {
 			if(err) {
 				console.log(err);
 				res.send(err);
 			} else {
-				console.log('deleteReport worked');
+				console.log('getReportsList worked');
 				res.status(200).send(res);
 			};
 		});
 	},
+
+	getAllData: function (req, res) {
+		var obj = {}
+		reportsService.getAllData(obj).then(function(err, res) {
+			if(err) {
+				console.log(err);
+				res.send(err);
+			} else {
+				console.log('getAllData worked');
+				res.status(200).send(res);
+			};
+		});
+	},
+
+	getRange: function (req, res) {
+		var obj = {}
+		reportsService.getRange(obj).then(function(err, res) {
+			if(err) {
+				console.log(err);
+				res.send(err);
+			} else {
+				console.log('getRange worked');
+				res.status(200).send(res);
+			};
+		});
+	};
 
 }
