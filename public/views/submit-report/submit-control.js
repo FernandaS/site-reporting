8a716b76d23ba4d0c2bc21449b2e945fd9a30f75
jacob-israel-turner <poppa.bear.joe@gmail.com>
2014-11-12 14:57:50 -0700

    Connects create new center to the service.

--- public/views/submit-report/submit-control.js
+++ public/views/submit-report/submit-control.js
@@ -1,6 +1,11 @@
 angular.module('lds-report')
 	.controller('submitReportCtrl', submitReportCtrl)
 
-function submitReportCtrl($scope){
-	
+function submitReportCtrl($scope, centerService){
+	$scope.test = 'Thisis a test.....'
+	$scope.submitCenter = function(){
+		centerService.create($scope.newCenter).then(function(data){
+			console.log(data);
+		})
+	}
 }
