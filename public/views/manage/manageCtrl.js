7064218dc97d7985c0b7c902f07467a6a0637b67
FernandaS <nandagms@gmail.com>
2014-11-13 14:40:38 -0700

    structure is ready, setting up ctrl

--- public/views/manage/manageCtrl.js
+++ public/views/manage/manageCtrl.js
@@ -2,4 +2,13 @@ var app = angular.module('lds-report');
 
 app.controller('manageCtrl', function($scope){
 
+var allCenters = function(){
+	centerService.getAll().
+	then(function(centers){
+		$scope.centers = centers.data
+	})
+}
+
+allCenters();
+
 });
