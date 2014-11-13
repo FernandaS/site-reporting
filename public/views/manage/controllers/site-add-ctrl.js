64fe1fbcea4253b3343308e62cfbbe02fb29e192
FernandaS <nandagms@gmail.com>
2014-11-13 16:44:45 -0700

    setting ng-models in for the add site section

--- public/views/manage/controllers/site-add-ctrl.js
+++ public/views/manage/controllers/site-add-ctrl.js
@@ -1,6 +1,19 @@
 var app = angular.module('lds-report');
 
-app.controller('siteAddCtrl', function($scope){
+app.controller('siteAddCtrl', function($scope,centerService){
+
+	function submitReportCtrl($scope, centerService){
+	$scope.yes = 'YES';
+	$scope.no = 'NO';
+	$scope.newCenter = {};
+	$scope.newCenter.active = 'YES';
+	$scope.test = 'Thisis a test.....'
+	$scope.submitCenter = function(){
+		centerService.create($scope.newCenter).then(function(data){
+			console.log(data);
+		})
+		// console.log($scope.newCenter)
+	}
 
 
 
