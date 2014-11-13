ec71a546ba48502af20d458e1c0cd7ef18a3f53e
Jason Turner <jasonalmaturner@gmail.com>
2014-11-13 16:40:24 -0700

    so close

--- public/main/main-control.js
+++ public/main/main-control.js
@@ -3,7 +3,6 @@ angular.module('lds-report')
 
 function mainCtrl($scope, authService){
 	function updateUser(){
-		
 		authService.getCurrentUser().then(function(data){
 			$scope.user = data.data;
 			console.log($scope.user);
