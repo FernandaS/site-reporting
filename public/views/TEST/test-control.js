b2648e8821bc6562c3401d28fb9128240ae98d6d
jacob-israel-turner <poppa.bear.joe@gmail.com>
2014-11-13 11:54:01 -0700

    Creates test page, SUCCESSFULLY implements Google charts in the test page. Cool.

--- public/views/TEST/test-control.js
+++ public/views/TEST/test-control.js
@@ -1,9 +1,24 @@
-angular.module('lds-report')
-	.controller('testCtrl', testCtrl);
+'use strict';
+
+google.load('visualization', '1.0', {'packages':['corechart']});
+google.setOnLoadCallback(function() {
+	angular.module('lds-report')
+		.controller('testCtrl', testCtrl);
+});
 
 function testCtrl($scope){
-	google.load('visualization', '1.0', {'packages':['corechart']});
-	// google.setOnLoadCallback(function(){
-	// 	console.log('Worked!')
-	// });
+	$scope.test = 'TESTTTTT'
+	var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+      ]);
+      var options = {
+        title: 'Company Performance'
+      };
+      var chart = new google.visualization.LineChart(document.getElementById('chartdiv'));
+ 
+      chart.draw(data, options);
 }
