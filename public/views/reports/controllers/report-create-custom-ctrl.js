4f677a19749f0e69fe90e3ba876957124b761e37
eklyman <ekknutson@gmail.com>
2014-11-14 11:08:34 -0700

    sets up reports controllers

---
+++ public/views/reports/controllers/report-create-custom-ctrl.js
@@ -0,0 +1,5 @@
+var app = angular.module('lds-report');
+
+app.controller('reportCreateCustomCtrl', function($scope, reportService, centerService){
+
+});
---
+++ public/views/reports/controllers/report-view-ctrl.js
@@ -0,0 +1,5 @@
+var app = angular.module('lds-report');
+
+app.controller('reportViewCtrl', function($scope, reportService, centerService){
+
+});
