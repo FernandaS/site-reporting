d0737d3d40a296a20ec1002a1ba28dea53b56250
eklyman <ekknutson@gmail.com>
2014-11-14 14:54:21 -0700

    adds new site report

--- public/views/reports/controllers/report-create-custom-ctrl.js
+++ public/views/reports/controllers/report-create-custom-ctrl.js
@@ -2,4 +2,19 @@ var app = angular.module('lds-report');
 
 app.controller('reportCreateCustomCtrl', function($scope, reportService, centerService){
 
+	$scope.reports = [];
+	$scope.years = [2011, 2012, 2013, 2014, 2015];
+	$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	$scope.centers = [{name:'Temple Square'}];
+
+	var getAllCenters = function(){
+		centerService.getAll()
+			.then(function(response){
+				$scope.centers = response.data;
+				console.log($scope.centers);
+			})
+	}
+
+	getAllCenters();
+
 });
--- public/views/reports/controllers/report-view-ctrl.js
+++ public/views/reports/controllers/report-view-ctrl.js
@@ -4,7 +4,21 @@ app.controller('reportViewCtrl', function($scope, reportService, centerService){
 
 	$scope.deleteReportWarning = false;
 	
-	$scope.deleteReport = function(){
+	$scope.center = $scope.$parent.selectedCenter; 
+	$scope.month = $scope.$parent.selectedMonth;
+	$scope.year = $scope.$parent.selectedYear;
+
+	// var getCenterReport = function(center, date){
+	// 	reportService.getOneBy($scope.center, $scope.date)
+	// }
+
+	// getCenterReport($scope.center, $scope.date);
+
+	$scope.deleteReportAlert = function(){
 		$scope.deleteReportWarning = !$scope.deleteReportWarning;
 	}
+
+	// $scope.deleteReport = function(id){
+	// 	reportService.delete(id)
+	// }
 });
