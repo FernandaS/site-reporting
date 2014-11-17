cafcf064142bca855d4842cb1f06408eeebafe9e
Jason Turner <jasonalmaturner@gmail.com>
2014-11-17 14:59:55 -0700

    Adding authentication and authorisation to endpoints. Will test as soon as I install bcrypt

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/api/logout', function(req, res){
 	req.logout();
 	res.redirect('#/login');
 });
-app.get('/download', reportsCtrl.sendReport);
+app.get('/download', middleware.requireAuth, middleware.ensureAdmin, reportsCtrl.sendReport);
 //passport.use(new LocalStrategy());
 app.listen(port, function(){
 	console.log('Listening at ' + port);
