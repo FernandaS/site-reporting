0f62431c1c8b35ffa8964071a86fb35c36eaf427
jacob-israel-turner <poppa.bear.joe@gmail.com>
2014-11-17 15:11:40 -0700

    Working on table.

--- public/views/reports/month-total/month-total-controller.js
+++ public/views/reports/month-total/month-total-controller.js
@@ -2,18 +2,25 @@ angular.module('lds-report')
 	.controller('monthTotalCtrl', monthTotalCtrl);
 
 function monthTotalCtrl($scope, $routeParams, reportService){
+
 	var splitDate = $routeParams.month.split('-');
 	$scope.date = new Date(splitDate[0], splitDate[1] - 1);
-	$scope.lastYear = new Date(splitDate[0]-1, splitDate[1]-1)
+	$scope.lastYear = new Date(splitDate[0]-1, splitDate[1]-1);
+
+
 	reportService.getAllBy($routeParams.month).then(function(data){
-		$scope.reports = data.data;
-		console.log($scope.reports)
+		$scope.thisYearReports = data.data;
+
+		for (var i = $scope.thisYearReports.length - 1; i >= 0; i--) {
+			$scope.thisYearReports[i]
+		};
+
+		var lastYear = $routeParams.month.split('-');
+		lastYear[0] -= 1;
+		lastYear = lastYear.join('-');
+		reportService.getAllBy(lastYear).then(function(data){
+			$scope.lastYearReports = data.data;
+			console.log($scope.lastYearReports)
+		})
 	})
-	var lastYear = $routeParams.month.split('-');
-	lastYear[0] -= 1;
-	lastYear = lastYear.join('-');
-	console.log(lastYear);
-	reportService.getAllBy(lastYear).then(function(data){
-		console.log(data);
-	})
-}
+};
