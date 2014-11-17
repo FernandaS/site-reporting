00a395022b5b35e357240a044012b77a3161c84b
eklyman <ekknutson@gmail.com>
2014-11-17 09:45:46 -0700

    reports view

--- public/views/reports/controllers/report-add-site-ctrl.js
+++ public/views/reports/controllers/report-add-site-ctrl.js
@@ -3,13 +3,8 @@ var app = angular.module('lds-report');
 app.controller('reportAddSiteCtrl', function($scope, reportService, centerService){
 
 	$scope.reports = [];
-	$scope.years = [2011, 2012, 2013, 2014, 2015];
-	$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	$scope.centers = [{name:'Temple Square'}];
+	$scope.centers = $scope.$parent.centers; 
 
-	// $scope.newReport.date = $scope.selectedMonth + ' 1, ' + $scope.selectedYear
-	// 		console.log($scope.newReport.date);
-	// 	}
 /*
 The getAllCenters could possibly happen in reportsCtrl.
 That way the centers can be shared between all of the controllers
@@ -18,15 +13,6 @@ reportAddSiteCtrl will inherit the scope from reportsCtrl and
 have access to the center data.  It might make it a tad bit
 cleaner.
 */
-	var getAllCenters = function(){
-		centerService.getAll()
-			.then(function(response){
-				$scope.centers = response.data;
-				console.log($scope.centers);
-			})
-	}
-
-	getAllCenters();
 
 	$scope.addNewSiteReport = function(newReport){
 		console.log($scope.newReport);
