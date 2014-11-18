4a41ec9ce103b4d2eaf65e4023797e1cee9daded
Jason Turner <jasonalmaturner@gmail.com>
2014-11-18 15:24:36 -0700

    adding authentication and authorisation middleware

--- server.js
+++ server.js
@@ -19,10 +19,12 @@ bcrypt = require('bcrypt');
 passport.use(new LocalStrategy(function(username, pass, done) {
 	userService.getUser(username).then(function (user) {
 		if (!user) {
+			console.log('Unknown user' + username);
 			return done(null, false, { message: 'Unknown user ' + username });
 		}
 		bcrypt.compare(pass, user.password, function(err, res) {
 			if (!res){
+				console.log('Invalid password');
 				return done(null, false, { message: 'Invalid password' });
 			}
 			return done(null, user);			
@@ -47,30 +49,30 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Center app
-app.get('/api/centers/:id'/*, middleware.requireAuth*/, centersCtrl.getCenter);
-app.get('/api/centers/'/*, middleware.requireAuth, middleware.ensureAdmin*/, centersCtrl.getAll);
-app.post('/api/centers'/*, middleware.requireAuth, middleware.ensureAdmin*/, centersCtrl.addCenter);
-app.put('/api/centers/:id'/*, middleware.requireAuth, middleware.ensureAdmin*/, centersCtrl.putCenter);
-app.delete('/api/centers/:id'/*, middleware.requireAuth, middleware.ensureAdmin*/, centersCtrl.deleteCenter);
+app.get('/api/centers/:id', middleware.requireAuth, centersCtrl.getCenter);
+app.get('/api/centers/', middleware.requireAuth, middleware.ensureAdmin, centersCtrl.getAll);
+app.post('/api/centers', middleware.requireAuth, middleware.ensureAdmin, centersCtrl.addCenter);
+app.put('/api/centers/:id', middleware.requireAuth, middleware.ensureAdmin, centersCtrl.putCenter);
+app.delete('/api/centers/:id', middleware.requireAuth, middleware.ensureAdmin, centersCtrl.deleteCenter);
 
 // User apis
 app.get('/api/users/me', function(req, res){
 	res.json(req.user);
 });
-app.get('/api/users/:id'/*, middleware.requireAuth*/, usersCtrl.getUser);
-app.get('/api/users'/*, middleware.requireAuth, middleware.ensureAdmin*/, usersCtrl.getAllUsers);
-app.post('/api/users'/*, middleware.requireAuth, middleware.ensureAdmin*/, usersCtrl.addUser);
-app.put('/api/users/:id'/*, middleware.requireAuth*/, usersCtrl.putUser);
-app.delete('/api/users/:id'/*, middleware.requireAuth, middleware.ensureAdmin*/, usersCtrl.deleteUser);
+app.get('/api/users/:id', middleware.requireAuth, usersCtrl.getUser);
+app.get('/api/users', middleware.requireAuth, middleware.ensureAdmin, usersCtrl.getAllUsers);
+app.post('/api/users', middleware.requireAuth, middleware.ensureAdmin, usersCtrl.addUser);
+app.put('/api/users/:id', middleware.requireAuth, usersCtrl.putUser);
+app.delete('/api/users/:id', middleware.requireAuth, middleware.ensureAdmin, usersCtrl.deleteUser);
 
 // Report apis, will add the apis with params after I figure it out. Or Aaron figures it out.
 
-app.get('/api/reports/allBy'/*, middleware.requireAuth, middleware.ensureAdmin*/, reportsCtrl.getAllBy); //month
-app.get('/api/reports/allFrom'/*, middleware.requireAuth, middleware.ensureAdmin*/, reportsCtrl.getAllFrom); //month
-app.get('/api/reports/oneBy/:id'/*, middleware.requireAuth*/, reportsCtrl.getOneBy); //center and month
-app.post('/api/reports'/*, middleware.requireAuth*/, reportsCtrl.addReport);
-app.put('/api/reports/:id'/*, middleware.requireAuth*/, reportsCtrl.editReport);
-app.delete('/api/reports/:id'/*, middleware.requireAuth, middleware.ensureAdmin*/, reportsCtrl.deleteReport);
+app.get('/api/reports/allBy', middleware.requireAuth, middleware.ensureAdmin, reportsCtrl.getAllBy); //month
+app.get('/api/reports/allFrom', middleware.requireAuth, middleware.ensureAdmin, reportsCtrl.getAllFrom); //month
+app.get('/api/reports/oneBy/:id', middleware.requireAuth, reportsCtrl.getOneBy); //center and month
+app.post('/api/reports', middleware.requireAuth, reportsCtrl.addReport);
+app.put('/api/reports/:id', middleware.requireAuth, reportsCtrl.editReport);
+app.delete('/api/reports/:id', middleware.requireAuth, middleware.ensureAdmin, reportsCtrl.deleteReport);
 
 // Additional emails
 app.post('/api/addlEmails/:id', emailsCtrl.addEmail);
